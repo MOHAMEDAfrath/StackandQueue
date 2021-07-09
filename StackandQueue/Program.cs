8641233ed339db5f53ebef40c743afa0a82a9dba
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-09 20:53:01 +0530

    [Afrath] Updated the file with push operation

--- StackandQueue/Program.cs
+++ StackandQueue/Program.cs
@@ -7,6 +7,23 @@ namespace StackandQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue Problem");
+            StackOperations stackOperations = new StackOperations();
+            Console.WriteLine("Enter 1 for Stack Operations");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
+            {
+                case 1:
+                    Console.WriteLine("Push");
+                    stackOperations.Push(70);
+                    stackOperations.Push(30);
+                    stackOperations.Push(56);
+                    Console.WriteLine("After pushing:");
+                    stackOperations.Display();
+                    break;
+
+            }
+           
+
         }
     }
 }
---
+++ StackandQueue/StackOperations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackandQueue
+{
+    class StackOperations
+    {
+        Node top = null;
+        public void Push(int data)
+        {
+            Node newnode = new Node(data);
+            if(top == null)
+            {
+                newnode.next = null;
+            }
+            else
+            {
+                newnode.next = this.top;
+            }
+            this.top = newnode;
+            Console.WriteLine("Successfully Inserted {0}",data);
+
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data+ " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
