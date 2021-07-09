15a1d8909baf2dc565ee54f38067ebd08269276d
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-09 21:23:47 +0530

    [Afrath] Updated the file with Queue Enqueue

--- StackandQueue/Program.cs
+++ StackandQueue/Program.cs
@@ -8,6 +8,7 @@ namespace StackandQueue
         {
             Console.WriteLine("Welcome to Stack and Queue Problem");
             StackOperations stackOperations = new StackOperations();
+            QueueOperations queueOperations = new QueueOperations();
             Console.WriteLine("Enter 1 for Stack Operations");
             int options = Convert.ToInt32(Console.ReadLine());
             switch (options)
@@ -44,6 +45,26 @@ namespace StackandQueue
                             case 4:
                                 return;
                         }
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Queue Operations");
+                    Console.WriteLine("1.Enqueue");
+                    Console.WriteLine("2.Peek");
+                    Console.WriteLine("3.Pop");
+                    Console.WriteLine("4.Exit");
+                    int qoptions = Convert.ToInt32(Console.ReadLine());
+                    switch (qoptions)
+                    {
+                        case 1:
+                            Console.WriteLine("Enqueue");
+                            queueOperations.Enqueue(56);
+                            queueOperations.Enqueue(30);
+                            queueOperations.Enqueue(70);
+                            Console.WriteLine("After Enqueue");
+                            queueOperations.Display();
+                            break;
+
                     }
                     break;
                 case 3:
---
+++ StackandQueue/QueueOperations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackandQueue
+{
+    class QueueOperations
+    {
+        Node front = null,rear = null;
+        public void Enqueue(int data)
+        {
+            Node newnode = new Node(data);
+            if(this.front == null)
+            {
+                newnode.next = null;
+                this.front = newnode;
+                this.rear = newnode;
+            }
+            else
+            {
+                this.rear.next = newnode;
+                this.rear = newnode;
+            }
+            Console.WriteLine("Enqueued {0}",data);
+        }
+        public void Display()
+        {
+            Node temp = this.front;
+            while (temp != null)
+            {
+                Console.Write(temp.data+ " ");
+                temp = temp.next;
+            }
+            Console.WriteLine(" ");
+        }
+
+    }
+}
